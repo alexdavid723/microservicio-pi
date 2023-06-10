10d1a7c0d086fbba2da2ad4867b9c3ca6c033b51
alexdavid723 <alexdavidxela@gmail.com>
2023-06-11 01:01:58 +0200

    registry

--- registry-serve/src/main/java/com/example/registryserve/RegistryServeApplication.java
+++ registry-serve/src/main/java/com/example/registryserve/RegistryServeApplication.java
@@ -1,14 +1,15 @@
-package com.example.registryserver;
+package com.example.registryserve;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistryServerApplication {
+public class RegistryServeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServeApplication.class, args);
+    }
 
 }
--- registry-serve/src/test/java/com/example/registryserve/RegistryServeApplicationTests.java
+++ registry-serve/src/test/java/com/example/registryserve/RegistryServeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.registryserver;
+package com.example.registryserve;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegistryServerApplicationTests {
+class RegistryServeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
