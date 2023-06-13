1d4c2d3a4fbb674c9ac640bde179bb7bfd6190e8
alexdavid723 <alexdavidxela@gmail.com>
2023-06-13 02:04:20 +0200

    aaaat

--- Producto/src/main/java/com/prooduct/producto/controllers/ProductoController.java
+++ Producto/src/main/java/com/prooduct/producto/controllers/ProductoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/producto")
-@CrossOrigin(origins="http://localhost:4200/")
 public class ProductoController {
     @Autowired
     ProductoServiceimpl productoService;
